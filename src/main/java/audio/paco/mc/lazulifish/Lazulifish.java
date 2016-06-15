29021d2d7db3312dd4462a864abc32056e0509b8
Austin Paco DeArmond <hello@paco.audio>
2016-06-15 14:34:40 -0500

    Initial commit

---
+++ src/main/java/audio/paco/mc/lazulifish/Lazulifish.java
@@ -0,0 +1,15 @@
+package audio.paco.mc.lazulifish;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lazulifish extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
