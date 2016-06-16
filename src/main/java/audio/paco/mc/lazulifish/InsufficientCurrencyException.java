87ab952aabb4eb28386483763541afca2189519f
Austin Paco DeArmond <hello@paco.audio>
2016-06-15 20:02:56 -0500

    Add "scare" command

---
+++ src/main/java/audio/paco/mc/lazulifish/InsufficientCurrencyException.java
@@ -0,0 +1,7 @@
+package audio.paco.mc.lazulifish;
+
+/**
+ * Created by ajdearmond on 6/15/2016.
+ */
+public class InsufficientCurrencyException {
+}
---
+++ src/main/java/audio/paco/mc/lazulifish/command/ScareCommand.java
@@ -0,0 +1,7 @@
+package audio.paco.mc.lazulifish.command;
+
+/**
+ * Created by ajdearmond on 6/15/2016.
+ */
+public class ScareCommand {
+}
